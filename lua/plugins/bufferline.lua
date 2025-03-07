9a1ca7f96e40f8ab7bc6925dc11db8d3c5f534fb
AlphaNumericPencil <Rohn2024@gmail.com>
2025-03-07 09:59:21 -0500

    Alter bufferline to fix closing all buffers on <leader>q.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -22,6 +22,7 @@ return {
         })
         vim.keymap.set('n', '<C-PageUp>', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Previous buffer', silent = true })
         vim.keymap.set('n', '<C-PageDown>', '<cmd>BufferLineCycleNext<CR>', { desc = 'Next buffer', silent = true })
-        vim.keymap.set('n', '<leader>q', '<cmd>bd<CR>', { desc = 'Close current buffer', silent = true })
+        vim.keymap.set('n', '<leader>q', '<cmd>bp|bd #<CR>', { desc = 'Close current buffer', silent = true })
+--[[ vim.keymap.set('n', '<leader>q', '<cmd>bd<CR>', { desc = 'Close current buffer', silent = true }) ]]
     end,
 }
