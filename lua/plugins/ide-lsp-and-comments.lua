6b3f0ca64250518d6a1dfb1e63383d19fbe5a0d2
AlphaNumericPencil <Rohn2024@gmail.com>
2025-04-01 09:39:35 -0400

    Fix: treesitter comment hook readded

--- lua/plugins/ide-lsp-and-comments.lua
+++ lua/plugins/ide-lsp-and-comments.lua
@@ -13,8 +13,22 @@ return {
         config = function()
           require('Comment').setup({
             pre_hook = function(ctx)
-              -- [Keep your existing hook configuration here]
-            end,
+              local U = require("Comment.utils")
+              local ts_utils = require("ts_context_commentstring.utils")
+              local ts_internal = require("ts_context_commentstring.internal")
+
+              local location = nil
+              if ctx.ctype == U.ctype.block then
+                location = ts_utils.get_cursor_location()
+              elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+                location = ts_utils.get_visual_start_location()
+              end
+
+              return ts_internal.calculate_commentstring({
+                key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+                location = location,
+              })
+            end, -- [Keep your existing hook configuration here]
           })
         end,
       },
