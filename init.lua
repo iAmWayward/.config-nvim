a9374277f2a003b74b6b63aa0174219da0b0b875
AlphaNumericPencil <Rohn2024@gmail.com>
2025-04-23 11:59:48 -0400

    Re-add cursor smear plugin

--- init.lua
+++ init.lua
@@ -8,8 +8,8 @@ vim.o.signcolumn = "yes"
 
 vim.cmd.set = "termguicolors"
 
-vim.opt.tabstop = 4     -- Number of spaces a TAB displays as
-vim.opt.shiftwidth = 4  -- Number of spaces for auto-indent and >>/<< operations
+vim.opt.tabstop = 4 -- Number of spaces a TAB displays as
+vim.opt.shiftwidth = 4 -- Number of spaces for auto-indent and >>/<< operations
 vim.opt.softtabstop = 4 -- Number of spaces for <Tab> key in insert mode
-vim.o.shell = "fish"
+vim.o.shell = "/usr/bin/fish"
 -- vim.opt.expandtab = true  -- Convert tabs to spaces
--- lua/plugins/feature-plugins.lua
+++ lua/plugins/feature-plugins.lua
@@ -222,8 +222,8 @@ return {
 			-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
 			-- refer to `:h file-pattern` for more examples
 			"BufReadPre "
-				.. vim.fn.expand("~")
-				.. "/Documents/Obsidian Vault/*.md",
+			.. vim.fn.expand("~")
+			.. "/Documents/Obsidian Vault/*.md",
 			"BufNewFile " .. vim.fn.expand("~") .. "/Obsidian Vault/*.md",
 		},
 		dependencies = {
@@ -299,79 +299,79 @@ return {
 			},
 		},
 	},
-	-- {
-	-- 	"sphamba/smear-cursor.nvim",
-	-- 	lazy = false,
-	-- 	opts = {
-	-- 		smear_between_buffers = false,
-	-- 		scroll_buffer_space = false,
-	-- 		smear_between_neighbor_lines = true, -- Smear when moving to adjacent lines
-	-- 		-- legacy_computing_symbols_support = true,
-	-- 		never_draw_over_target = false,
-	-- 		vertical_bar_cursor = false,
-	-- 		vertical_bar_cursor_insert_mode = true,
-	-- 		min_horizontal_distance_smear = 2,
-	-- 		min_vertical_distance_smear = 2,
-	-- 		-- Attempt to hide the real cursor by drawing a character below it.
-	-- 		-- Can be useful when not using `termguicolors`
-	-- 		-- hide_target_hack = true,
-	--
-	-- 		max_kept_windows = 80, --default is 50 Number of windows that stay open for rendering the effect.
-	-- 		-- Sets animation framerate
-	-- 		time_interval = 17, -- Sets animation framerate in milliseconds. default 17 milliseconds
-	--
-	-- 		-- Amount of time the cursor has to stay still before triggering animation.
-	-- 		-- Useful if the target changes and rapidly comes back to its original position.
-	-- 		-- E.g. when hitting a keybinding that triggers CmdlineEnter.
-	-- 		-- Increase if the cursor makes weird jumps when hitting keys.
-	-- 		delay_event_to_smear = 3, -- milliseconds
-	--
-	-- 		-- Delay for `vim.on_key` to avoid redundancy with vim events triggers.
-	-- 		delay_after_key = 5, -- milliseconds
-	--
-	-- 		-- Smear configuration ---------------------------------------------------------
-	--
-	-- 		-- How fast the smear's head moves towards the target.
-	-- 		-- 0: no movement, 1: instantaneous
-	-- 		stiffness = 0.65,
-	--
-	-- 		-- How fast the smear's tail moves towards the target.
-	-- 		-- 0: no movement, 1: instantaneous
-	-- 		trailing_stiffness = 0.49, -- 0.3,
-	--
-	-- 		-- Controls if middle points are closer to the head or the tail.
-	-- 		-- < 1: closer to the tail, > 1: closer to the head
-	-- 		trailing_exponent = 1, -- default 2
-	--
-	-- 		-- How much the smear slows down when getting close to the target.
-	-- 		-- < 0: less slowdown, > 0: more slowdown. Keep small, e.g. [-0.2, 0.2]
-	-- 		slowdown_exponent = 0,
-	--
-	-- 		-- Stop animating when the smear's tail is within this distance (in characters) from the target.
-	-- 		distance_stop_animating = 0.15,
-	--
-	-- 		-- Set of parameters for insert mode
-	-- 		stiffness_insert_mode = 0.4,
-	-- 		trailing_stiffness_insert_mode = 0.4,
-	-- 		trailing_exponent_insert_mode = 1,
-	-- 		distance_stop_animating_vertical_bar = 0.875, -- Can be decreased (e.g. to 0.1) if using legacy computing symbols
-	--
-	-- 		-- When to switch between rasterization methods
-	-- 		max_slope_horizontal = 0.5,
-	-- 		min_slope_vertical = 2,
-	--
-	-- 		-- color_levels = 16,                   -- Minimum 1, don't set manually if using cterm_cursor_colors
-	-- 		gamma = 2.2,                      -- For color blending
-	-- 		max_shade_no_matrix = 0.75,       -- 0: more overhangs, 1: more matrices
-	-- 		matrix_pixel_threshold = 0.7,     -- 0: all pixels, 1: no pixel
-	-- 		matrix_pixel_threshold_vertical_bar = 0.3, -- 0: all pixels, 1: no pixel
-	-- 		matrix_pixel_min_factor = 0.5,    -- 0: all pixels, 1: no pixel
-	-- 		volume_reduction_exponent = 0.3,  -- 0: no reduction, 1: full reduction
-	-- 		minimum_volume_factor = 0.7,      -- 0: no limit, 1: no reduction
-	-- 		max_length = 60,                  -- 35,                           -- Maximum smear length
-	-- 		max_length_insert_mode = 1,
-	-- 	},
-	-- },
+	{
+		"sphamba/smear-cursor.nvim",
+		lazy = false,
+		opts = {
+			smear_between_buffers = false,
+			scroll_buffer_space = false,
+			smear_between_neighbor_lines = true, -- Smear when moving to adjacent lines
+			-- legacy_computing_symbols_support = true,
+			never_draw_over_target = false,
+			vertical_bar_cursor = false,
+			vertical_bar_cursor_insert_mode = true,
+			min_horizontal_distance_smear = 2,
+			min_vertical_distance_smear = 2,
+			-- Attempt to hide the real cursor by drawing a character below it.
+			-- Can be useful when not using `termguicolors`
+			-- hide_target_hack = true,
+
+			max_kept_windows = 80, --default is 50 Number of windows that stay open for rendering the effect.
+			-- Sets animation framerate
+			time_interval = 17, -- Sets animation framerate in milliseconds. default 17 milliseconds
+
+			-- Amount of time the cursor has to stay still before triggering animation.
+			-- Useful if the target changes and rapidly comes back to its original position.
+			-- E.g. when hitting a keybinding that triggers CmdlineEnter.
+			-- Increase if the cursor makes weird jumps when hitting keys.
+			delay_event_to_smear = 3, -- milliseconds
+
+			-- Delay for `vim.on_key` to avoid redundancy with vim events triggers.
+			delay_after_key = 5, -- milliseconds
+
+			-- Smear configuration ---------------------------------------------------------
+
+			-- How fast the smear's head moves towards the target.
+			-- 0: no movement, 1: instantaneous
+			stiffness = 0.65,
+
+			-- How fast the smear's tail moves towards the target.
+			-- 0: no movement, 1: instantaneous
+			trailing_stiffness = 0.49, -- 0.3,
+
+			-- Controls if middle points are closer to the head or the tail.
+			-- < 1: closer to the tail, > 1: closer to the head
+			trailing_exponent = 1, -- default 2
+
+			-- How much the smear slows down when getting close to the target.
+			-- < 0: less slowdown, > 0: more slowdown. Keep small, e.g. [-0.2, 0.2]
+			slowdown_exponent = 0,
+
+			-- Stop animating when the smear's tail is within this distance (in characters) from the target.
+			distance_stop_animating = 0.15,
+
+			-- Set of parameters for insert mode
+			stiffness_insert_mode = 0.4,
+			trailing_stiffness_insert_mode = 0.4,
+			trailing_exponent_insert_mode = 1,
+			distance_stop_animating_vertical_bar = 0.875, -- Can be decreased (e.g. to 0.1) if using legacy computing symbols
+
+			-- When to switch between rasterization methods
+			max_slope_horizontal = 0.5,
+			min_slope_vertical = 2,
+
+			-- color_levels = 16,                   -- Minimum 1, don't set manually if using cterm_cursor_colors
+			gamma = 2.2,                      -- For color blending
+			max_shade_no_matrix = 0.75,       -- 0: more overhangs, 1: more matrices
+			matrix_pixel_threshold = 0.7,     -- 0: all pixels, 1: no pixel
+			matrix_pixel_threshold_vertical_bar = 0.3, -- 0: all pixels, 1: no pixel
+			matrix_pixel_min_factor = 0.5,    -- 0: all pixels, 1: no pixel
+			volume_reduction_exponent = 0.3,  -- 0: no reduction, 1: full reduction
+			minimum_volume_factor = 0.7,      -- 0: no limit, 1: no reduction
+			max_length = 60,                  -- 35,                           -- Maximum smear length
+			max_length_insert_mode = 1,
+		},
+	},
 	{
 		"rcarriga/nvim-notify",
 		lazy = false,
@@ -1004,7 +1004,7 @@ return {
 		opts = {
 			direction = "horizontal", -- Opens at the bottom
 			open_mapping = [[<c-\>]], -- Toggle with Ctrl+\ (default)
-			size = 15, -- Height of the terminal split
+			size = 15,       -- Height of the terminal split
 			persist_size = false,
 			shade_terminals = true,
 			insert_mappings = false, -- Disable default insert mode mappings
