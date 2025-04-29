8c0f89b1dbf3fc290fb6c62579dda771010c8d62
AlphaNumericPencil <Rohn2024@gmail.com>
2025-04-29 17:38:15 -0400

    Baby sync

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ require("config.lazy")
 vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
--- vim.o.signcolumn = "yes"
+
 vim.o.signcolumn = "yes"
 vim.o.number = true
 
@@ -15,3 +15,5 @@ vim.opt.expandtab = true -- Convert tabs to spaces
 vim.opt.tabstop = 2      -- Number of spaces a TAB displays as
 vim.opt.shiftwidth = 2   -- Number of spaces for auto-indent and >>/<< operations
 vim.opt.softtabstop = 2  -- Number of spaces for <Tab> key in insert mode
+
+vim.opt.linebreak = true
