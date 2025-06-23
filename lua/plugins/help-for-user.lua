e11a8155376919744c53ef525ba148514aaa54d0
Wayward <adigitaldruid@proton.me>
2025-06-22 22:04:41 -0400

    ccls updates

--- lua/plugins/help-for-user.lua
+++ lua/plugins/help-for-user.lua
@@ -11,4 +11,46 @@ return {
 			},
 		},
 	},
+	{
+		"mrjones2014/legendary.nvim",
+		keys = {
+			{ "<C-l>", "<cmd>Legendary<cr>", desc = "Open Command Palette" },
+		},
+		dependencies = {
+			"kkharji/sqlite.lua",
+			"folke/which-key.nvim",
+		},
+		config = function()
+			require("legendary").setup({
+				include_builtin = true,
+				include_legendary_cmds = true,
+				extensions = {
+					which_key = {
+						-- auto_register = {
+						-- 	neotree = true,
+						-- 	neo_tree = true,
+						-- 	["neo-tree"] = true,
+						-- },
+						auto_register = true,
+						do_binding = false,
+						use_groups = true,
+					},
+				},
+			})
+
+			-- Load all regular keymaps
+			local keymaps = require("config.keymaps")
+			require("legendary").keymaps(keymaps.items)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "neo-tree",
+				callback = function()
+					require("legendary").keymaps(require("config.keymaps").items, {
+						buffer = true,
+						filetype = "neo-tree",
+					})
+				end,
+			})
+		end,
+	},
 }
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -5,50 +5,58 @@
 --- WARNING:
 _G.__cached_neo_tree_selector = nil
 _G.__get_selector = function()
-  return _G.__cached_neo_tree_selector
+	return _G.__cached_neo_tree_selector
 end
 return {
-  --============================== Core Plugins ==============================--
-  -- { "pandasoli/nekovim" },
-  -- Show people on discord
-  { "andweeb/presence.nvim" },
-  {
-    "https://github.com/adelarsq/neovcs.vim",
-    lazy = true,
-    keys = {
-      "<leader>v",
-    },
-    config = function()
-      require("neovcs").setup()
-    end,
-  },
-  {
-    "HugoBde/subversigns.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "kawre/leetcode.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      -- "ibhagwan/fzf-lua",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-    cmd = "Leet", -- Load only on command
-    opts = {
-      -- configuration goes here
-    },
-  },
-  {
-    "m4xshen/hardtime.nvim",
+	--============================== Core Plugins ==============================--
+	-- { "pandasoli/nekovim" },
+	-- Develop neovim plugins
+	{
+		"jmbuhr/otter.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {},
+	},
+	-- Show people on discord
+	{ "andweeb/presence.nvim" },
+	{
+		"https://github.com/adelarsq/neovcs.vim",
+		lazy = true,
+		keys = {
+			"<leader>v",
+		},
+		config = function()
+			require("neovcs").setup()
+		end,
+	},
+	{
+		"HugoBde/subversigns.nvim",
+		event = "VeryLazy",
+	},
+	{
+		"kawre/leetcode.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			-- "ibhagwan/fzf-lua",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		cmd = "Leet", -- Load only on command
+		opts = {
+			-- configuration goes here
+		},
+	},
+	{
+		"m4xshen/hardtime.nvim",
 
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {
 
-      lazy = false,
-      enabled = false,
-      restriction_mode = "hint",
-    },
-  },
+			lazy = false,
+			enabled = false,
+			restriction_mode = "hint",
+		},
+	},
 }
