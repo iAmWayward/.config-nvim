9d69bd3847dd8631aeef861c8d6ede53f6753c47
iAmWayward <1996lad@pm.me>
2026-08-07 15:18:59 -0400

    Re-implement scratchview

--- lua/plugins/scratchview.lua
+++ lua/plugins/scratchview.lua
@@ -1,19 +1,19 @@
 return {
-  -- {
-  --   "iAmWayward/Scratchview",
-  --   main = "scratchview",
-  --   lazy = false,
-  --   branch = "volt-up",
-  --   priority = 100,
-  --   opts = {
-  --     priority = { "readme", "git_log", "directory" },
-  --     -- views = {
-  --     --   readme    = { ... },
-  --     --   git_log   = { ... },
-  --     --   directory = { ... },
-  --     --   custom    = { { name = "todo", build = function(ctx) ... end } },
-  --     -- },
-  --     -- keymaps = { next_view = "<Tab>", prev_view = "<S-Tab>" },
-  --   },
-  -- },
+  {
+    "Im-Wayward/Scratchview",
+    main = "scratchview",
+    lazy = false,
+    branch = "volt-up",
+    priority = 100,
+    opts = {
+      priority = { "readme", "git_log", "directory" },
+      -- views = {
+      --   readme    = { ... },
+      --   git_log   = { ... },
+      --   directory = { ... },
+      --   custom    = { { name = "todo", build = function(ctx) ... end } },
+      -- },
+      -- keymaps = { next_view = "<Tab>", prev_view = "<S-Tab>" },
+    },
+  },
 }
